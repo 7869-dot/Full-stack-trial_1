3f33de7d29444b748057b6d852bac6213d6ed268
7869-dot <haani40651@gmail.com>
2025-12-18 19:02:56 +0400

    Frontend is done

--- fullstack_1/src/unauth/components/navBar.js
+++ fullstack_1/src/unauth/components/navBar.js
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 import logo from '../../assests/logo.jpg';
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <div className="navbar-logo">
+        <div className="navbar-logo" onClick={closeMenu}>
           <img src={logo} alt="tubestamp logo" className="logo-image" />
           <span className="logo-text">Prodstamp</span>
         </div>
-        <div className="navbar-links">
-          <a href="#pricing" className="nav-link">Pricing</a>
-          <a href="#blog" className="nav-link">Blog</a>
+        <button
+          className={`navbar-toggle ${isOpen ? 'open' : ''}`}
+          type="button"
+          aria-label="Toggle navigation menu"
+          aria-expanded={isOpen}
+          onClick={toggleMenu}
+        >
+          <span className="bar" />
+          <span className="bar" />
+          <span className="bar" />
+        </button>
+        <div className={`navbar-menu ${isOpen ? 'open' : ''}`}>
+          <div className="navbar-links">
+            <a href="#pricing" className="nav-link" onClick={closeMenu}>
+              Pricing
+            </a>
+            <a href="#blog" className="nav-link" onClick={closeMenu}>
+              Blog
+            </a>
+          </div>
+          <button className="navbar-cta" onClick={closeMenu}>
+            Do More With Video
+          </button>
         </div>
-        <button className="navbar-cta">Do More With Video</button>
       </div>
     </nav>
   );
